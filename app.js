21d5a58ab68fe4a59b30671151e5bc985a663aab
Kayot88 <kayot88@gmail.com>
2019-02-06 12:27:56 +0200

    login&register_backend

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const analyticsRoutes = require('./routes/analyticsRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const positionRoutes = require('./routes/positionRoutes');
+// const errorHandlers = require('./handlers/errorHandlers');
 const keys = require('./config/keys');
 const app = express();
 
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
 .then(() =>   console.log('MongoDB connected'))
 .catch(error => console.log(error))
 
@@ -28,6 +29,8 @@ app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
+
+// app.use(errorHandlers.notFound);
 
 
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,50 @@
-exports.login = (req, res) => {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
-    }
-    // login: req.body
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+
+exports.login = async (req, res) => {
+  const candida = await User.findOne({
+    email: req.body.email
   });
+  if (candida) {
+   const passwortRes = bcrypt.compareSync(req.body.password, candida.password)
+   console.log(passwortRes);
+   if (passwortRes) {
+    const token = jwt.sign({
+      email: candida.email,
+      userId: candida._id 
+    }, keys.jwt, {expiresIn: 60*60});
+    res.status(200).json({token: `Bearer ${token}`}) 
+   } else {
+     res.status(401).json({message: 'not equal password'})
+   };
+  } else {
+    res.status(404).json({
+      massage: 'user with this email not found'
+    });
+  }
 };
-exports.register = (req, res) => {
-  res.status(200).json({
-    "register": 'from controllers'
+
+exports.register = async (req, res) => {
+  const salt = bcrypt.genSaltSync(10);
+  const password = req.body.password;
+  const candida = await User.findOne({
+    email: req.body.email
   });
+  if (candida) {
+    res.status(409).json({
+      message: 'Her tam. Takoy zaniat'
+    });
+  } else {
+    try {
+      const user = new User({
+        email: req.body.email,
+        password: bcrypt.hashSync(password, salt)
+      }).save().then(console.log(`Adedd new`))
+    } catch (e) {
+      console.log(e);
+    }
+  }
 };
